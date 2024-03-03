d5858f1f7f9bb459b06c35e9760e203318dc593a
Tomas <tomasmeneses2012@gmail.com>
2024-03-03 14:49:09 +0000

    Non-italic comments (didnt work' )

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -4,5 +4,10 @@ return {
 	priority = 999, -- All plugins have priority 50 by default, load scheme before anything
 	config = function() -- Every plugin has a config which is either a function or a table, gets called when plugin is loaded
 		vim.cmd('colorscheme onedark')
+        require('onedark').setup {
+            code_style = {
+                comments = 'none'
+            }
+        }
 	end
 }
