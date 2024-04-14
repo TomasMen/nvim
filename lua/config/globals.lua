d5defef878ae2e30f2b2ec3664257c6333c32659
Tomas <tomasmeneses2012@gmail.com>
2024-04-14 22:57:05 +0100

    Something

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = True
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,6 +37,6 @@ vim.opt.iskeyword:append("-") -- Makes it so words separated by - are considered
 vim.opt.mouse:append('a')
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.modifiable = true -- Can edit current buffer
---vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+-- vim.opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 vim.opt.encoding = "UTF-8"
 
