11c2074abfa52b2648ec539ff7a96a2cf914926a
Tomas <tomasmeneses2012@gmail.com>
2024-04-21 16:48:51 +0100

    Indent highlight

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,5 +1,9 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
+    lazy = false,
     main = "ibl",
     opts = {},
+	config = function() -- Every plugin has a config which is either a function or a table, gets called when plugin is loaded
+		require("ibl").setup()
+	end
 }
